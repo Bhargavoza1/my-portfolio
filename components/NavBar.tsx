2183b2848301420f99363d932627691429722bed
Bhargavoza1 <hexlok777@gmail.com>
2023-09-05 11:24:00 +0530

    navbar line start from center

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,26 +1,25 @@
 "use client"
 import Link from "next/link";
-
 import {usePathname } from "next/navigation";
 
-const MyLink = ({href, title , className=""})=>{
-    const router = usePathname();
-
+const MyLink = (a:{href:string, title:string , className:string })=>{
+    const router:string = usePathname();
     return(
-        <Link href={href} className={`${className} relative group`}>
-            {title}
-            <span className={`h-[1px] inline-block  w-0 bg-black absolute left-0 -bottom-0.5
-                group-hover:w-full transition-all ease duration-300  ${router === href ? 'w-full' : 'w-0'} `} >
+        <Link href={a.href} className={`${a.className} relative group `} >
+            {a.title}
+
+            <span className={`h-[1px] inline-block  w-0 bg-black absolute -bottom-0.5 left-[50%] translate-x-[-50%]
+                group-hover:w-full transition-all ease duration-300  ${router === a.href ? 'w-full' : 'w-0'} `} >
                 &nbsp;
             </span>
         </Link>
     )
 }
 
-function NavBar( ) {
+function NavBar() {
     return (
         <div>
-        <header className='w-full px-32 py-8 font-medium  flex items-center justify-between'>
+        <header className='w-full px-32 py-8 font-medium  flex items-center justify-between '>
            <nav >
                <MyLink href="/" title="Home" className='mr-4'/>
                <MyLink href="/about" title="About" className='mx-4'/>
