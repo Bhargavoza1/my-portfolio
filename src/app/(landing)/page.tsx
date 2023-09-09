0f6ecaed29a936268a9ffc7cca21204639eed9d0
Bhargavoza1 <hexlok777@gmail.com>
2023-09-09 15:40:15 +0530

    back up home

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -7,9 +7,9 @@ const myfont = localFont({src:"../../components/fonts/ron-ron-regular-400.ttf"})
 
 export default function Home() {
   return (
-  <div className=' min-h-[65vh] myimg:min-h-[80vh] pt-0   md:container md:mx-auto myimg:flex content-center   items-center justify-evenly grid     place-items-center '>
+  <div className=' min-h-[65vh] myimg:min-h-[80vh] pt-0   md:container md:mx-auto myimg:flex content-center   items-center justify-center grid     place-items-center '>
 
-    <div className=' min-w-[400px]  place-items-center  ' style={{ borderRadius: '20%', overflow: 'hidden', width: '400px', height: '400px' , position:'relative'}}>
+    <div className=' myimg:ml-[5%]  min-w-[400px]  place-items-center  ' style={{ borderRadius: '50%', overflow: 'hidden', width: '400px', height: '400px' , position:'relative'}}>
       <Image
           src="/Bhargav.jpg"
           layout='fill'
@@ -19,7 +19,8 @@ export default function Home() {
     </div>
 
 
-    <div className=' px-5 myimg:px-10 pt-10 myimg:pt-0 text-justify   grid  place-items-center   [word-spacing:5px] tracking-widest' style=  {myfont.style}> I am computer engineer from india asdas asd asd  .
+    <div className='myimg:ml-[10%] px-5 myimg:px-10  pt-[60px] myimg:pt-0 text-justify   grid  place-items-center '   >
+      <h1 className='  text-4xl   ' style=  {myfont.style}>Hello, My Name is <span className='text-[#ffd646]' >Bhargav Oza.</span></h1>
     </div>
 
   </div>
