03aac7257aaa93ea5808fa14514d58dd594cd3d6
Bhargavoza1 <hexlok777@gmail.com>
2023-09-09 08:55:02 +0530

    backup

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
   <div className='  lg:pt-20 3xl:pt-40 md:container md:mx-auto myimg:flex content-center   items-center justify-center grid   place-items-center '>
 
-    <div className='place-items-center container' style={{width: '400px', height: '400px', position: 'relative'}}>
+    <div className='  place-items-center container' style={{ borderRadius: '20%', overflow: 'hidden', width: '400px', height: '400px' , position:'relative'}}>
       <Image
           src="/Bhargav.jpg"
           layout='fill'
@@ -18,7 +18,7 @@ export default function Home() {
     </div>
 
 
-    <div className=' text-justify  px-5 grid  place-items-center   [word-spacing:5px] tracking-widest' style=  {myfont.style}> I am computer engineer from india asdas asd.
+    <div className='  w-[50%] text-justify  px-10 grid  place-items-center   [word-spacing:5px] tracking-widest' style=  {myfont.style}> I am computer engineer from india.
     </div>
 
   </div>
