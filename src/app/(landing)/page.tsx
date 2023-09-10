bf93940aaccfecfd231d5520e527e43d8131ae9d
Bhargavoza1 <hexlok777@gmail.com>
2023-09-10 10:05:34 +0530

    Revert "backup"
    This reverts commit 49f1ff518fb72fdf4145a0ca39d87f96ab7c4b9b.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,13 +3,10 @@ import React from "react";
 import localFont from "next/font/local";
 const myfont = localFont({src:"../../components/fonts/ron-ron-regular-400.ttf"})
 
-//const myfont = localFont({src:"../../components/fonts/OTF/Satoshi-Bold.otf"})
 
-import {  } from 'next/font/google'
 
 export default function Home() {
   return (
-
   <div className=' min-h-[65vh] myimg:min-h-[80vh] pt-0   md:container md:mx-auto myimg:flex content-center   items-center justify-center grid     place-items-center '>
 
     <div className=' myimg:ml-[5%]  min-w-[400px]  place-items-center  ' style={{ borderRadius: '50%', overflow: 'hidden', width: '400px', height: '400px' , position:'relative'}}>
@@ -23,7 +20,7 @@ export default function Home() {
 
 
     <div className='myimg:ml-[10%] px-5 myimg:px-10  pt-[60px] myimg:pt-0 text-justify   grid  place-items-center '   >
-      <h1 className='  text-center text-3xl md:text-4xl   ' style=  {myfont.style}>Hello, My Name is <span className='text-[#085bb9] ' >Bhargav Oza.</span></h1>
+      <h1 className='  text-4xl   ' style=  {myfont.style}>Hello, My Name is <span className='text-[#ffd646]' >Bhargav Oza.</span></h1>
     </div>
 
   </div>
