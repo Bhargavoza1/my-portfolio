8df6a63ee2199dce688a6f43431922b8501661f0
Bhargavoza1 <hexlok777@gmail.com>
2023-09-20 15:49:14 +0530

    nav b border fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import {usePathname} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import {Menu} from "lucide-react";
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet"
+import {Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import localFont from "next/font/local";
 
 const myfont = localFont({src: "./fonts/cyber.bold-book.ttf"})
@@ -19,7 +19,7 @@ const MyLink = ({href, title, className, spanclass = ' '}: {
         <Link href={href} className={`${className}  relative group md:text-2xl  lg:text-3xl `} style={myfont.style}>
             {title}
 
-            <span className={`${spanclass} h-[2px] inline-block  w-0 bg-[#ffd646] absolute -bottom-0.5 left-[50%] translate-x-[-50%]
+            <span className={`${spanclass} h-[2px] inline-block  w-0 bg-[#ffd646] absolute -bottom-0.5 
                 group-hover:w-full transition-all ease duration-300  ${router === href ? 'w-full' : 'w-0'} `}>
                 &nbsp;
             </span>
@@ -36,11 +36,11 @@ function NavBar() {
 
             <div>
                 <nav className="hidden  myimg:px-32 py-[40px]   md:flex md:w-full        ">
-                    <MyLink href="/" title="Home" className='lg:mr-6 sm:mr-3.5' spanclass=' '/>
-                    <MyLink href="/about" title="About" className='lg:mx-6 sm:mx-3.5' spanclass=' '/>
-                    <MyLink href="/projects" title="Projects" className='lg:mx-6 sm:mx-3.5 ' spanclass=' '/>
-                    <MyLink href="/blogs" title="Blogs" className='lg:mx-6 sm:mx-3.5' spanclass=' '/>
-                    <MyLink href="/hireme" title="Hire&nbsp;Me" className='lg:ml-6 sm:ml-3.5' spanclass=' '/>
+                    <MyLink href="/" title="Home" className='lg:mr-6 sm:mr-3.5' spanclass=' left-[50%] translate-x-[-50%]'/>
+                    <MyLink href="/about" title="About" className='lg:mx-6 sm:mx-3.5' spanclass='left-[50%] translate-x-[-50%] '/>
+                    <MyLink href="/projects" title="Projects" className='lg:mx-6 sm:mx-3.5 ' spanclass='left-[50%] translate-x-[-50%] '/>
+                    <MyLink href="/blogs" title="Blogs" className='lg:mx-6 sm:mx-3.5' spanclass='left-[50%] translate-x-[-50%] '/>
+                    <MyLink href="/hireme" title="Hire&nbsp;Me" className='lg:ml-6 sm:ml-3.5' spanclass=' left-[50%] translate-x-[-50%]'/>
                 </nav>
             </div>
 
@@ -53,18 +53,18 @@ function NavBar() {
                     </SheetTrigger>
                     <SheetContent side='right'
                                   className='bg-[#ffd646]  p-0 border-0  data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right'>
-                        <div className='bg-[#1b96f3]     h-full text-black p-0'>
-                            <div className='   flex min-h-[80vh] items-center '>
+                        <div className='bg-[#1b96f3]     h-full text-[#1a1a1f] p-0'>
+                            <div className='   flex pt-[100px] items-center '>
                                 <nav className="grid   w-[10px]  space-y-10    ">
-                                    <MyLink href="/" title="Home" className='text-3xl  w-fit pl-1'
+                                    <MyLink href="/" title="Home" className='text-4xl  w-fit pl-1'
                                             spanclass='left-[0] translate-x-[0]'/>
-                                    <MyLink href="/about" title="About" className='text-3xl w-fit pl-1'
+                                    <MyLink href="/about" title="About" className='text-4xl w-fit pl-1'
                                             spanclass='left-[0] translate-x-[0]'/>
-                                    <MyLink href="/projects" title="Projects" className='text-3xl w-fit pl-1'
+                                    <MyLink href="/projects" title="Projects" className='text-4xl w-fit pl-1'
                                             spanclass='left-[0] translate-x-[0]'/>
-                                    <MyLink href="/blogs" title="Blogs" className='text-3xl w-fit pl-1'
+                                    <MyLink href="/blogs" title="Blogs" className='text-4xl w-fit pl-1'
                                             spanclass='left-[0] translate-x-[0]'/>
-                                    <MyLink href="/hireme" title="Hire&nbsp;Me" className='text-3xl w-fit pl-1'
+                                    <MyLink href="/hireme" title="Hire&nbsp;Me" className='text-4xl w-fit pl-1'
                                             spanclass='left-[0] translate-x-[0]'/>
                                 </nav>
                             </div>
