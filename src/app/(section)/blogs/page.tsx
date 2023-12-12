7bb3c92eabae89a89e279f385ced44bfcf533a98
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2023-12-12 19:55:28 +0530

    layout

--- src/app/(section)/blogs/page.tsx
+++ src/app/(section)/blogs/page.tsx
@@ -15,28 +15,20 @@ export const metadata: Metadata = {
 
 function About(): React.JSX.Element {
     return (
-        <div className='mt-24 md:mt-40 px-4 md:px-6  max-w-[1700px] mx-auto    pt-4 md:pb-4 sm:px-6  lg:px-8'>
+        <div className='mt-14 md:mt-24 px-4 md:px-6  max-w-[1700px] mx-auto    pt-4 md:pb-4 sm:px-6  lg:px-8'>
 
-
-            <div
-                className='     space-y-10   text-xl        place-content-around      '>
-
-                <div className='md:min-w-[300px]'>
-                    <h1 className={` ${styles.thickline} mt-8 my-3 text-5xl  tracking-tight font-extrabold   sm:text-5xl md:text-6xl  `}>
-                        Blogs
-                    </h1>
-
-                </div>
-                <div className='  '>
-                    <div className='text-lg md:text-2xl text-gray-400  '>Technology, thoughts and
-                        stories.
-                    </div>
-                </div>
-                <BlogPosts/>
+            <div className='md:min-w-[300px]'>
+                <h1 className={` ${styles.thickline} mt-8 my-3 text-5xl  tracking-tight font-extrabold   sm:text-5xl md:text-6xl  `}>
+                    Blogs
+                </h1>
             </div>
-
+            <div className=' pt-2  '>
+                <div className='text-lg md:text-2xl text-gray-400  '>Technology, thoughts and
+                    stories.
+                </div>
+            </div>
+            <BlogPosts/>
         </div>
-
 
     );
 }
--- src/app/(section)/projects/page.tsx
+++ src/app/(section)/projects/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectPosts from "@/components/ProjectPosts";
 import styles from "@/app/(section)/about/page.module.css";
 import SocialMedia from "@/components/SocialMedia";
-import type { Metadata } from 'next'
+import type {Metadata} from 'next'
 
 export const metadata: Metadata = {
     title: "Bhargav's Projects",
@@ -10,31 +10,22 @@ export const metadata: Metadata = {
 }
 
 export const revalidate = 10
-function About( ):React.JSX.Element {
+
+function About(): React.JSX.Element {
     return (
-        <div className=' mt-24 md:mt-40 px-4 md:px-6  max-w-[1700px] mx-auto    pt-4 md:pb-4 sm:px-6  lg:px-8   '>
-
-
-
-
-
-                        <div
-                            className='     space-y-10   text-xl       place-content-around      '>
-
-                            <div className='md:min-w-[300px]'>
-                                <h1 className={` ${styles.thickline} mt-8 my-3 text-5xl  tracking-tight font-extrabold   sm:text-5xl md:text-6xl  `}>
-                                    Projects
-                                </h1>
-
-                            </div>
-                            <div className='  '>
-                                <div className='text-lg md:text-2xl text-gray-400' >My main motive for making this website is to showcase my projects.
-                                </div>
-                            </div>
-                            <ProjectPosts/>
-                        </div>
-                    </div>
-
+        <div className=' mt-14 md:mt-24 px-4 md:px-6  max-w-[1700px] mx-auto    pt-4 md:pb-4 sm:px-6  lg:px-8   '>
+            <div className='md:min-w-[300px]'>
+                <h1 className={` ${styles.thickline} mt-8 my-3 text-5xl  tracking-tight font-extrabold   sm:text-5xl md:text-6xl  `}>
+                    Projects
+                </h1>
+            </div>
+            <div className=' pt-2 '>
+                <div className='text-lg md:text-2xl text-gray-400'>My main motive for making this website is to showcase
+                    my projects.
+                </div>
+            </div>
+            <ProjectPosts/>
+        </div>
     );
 }
 
