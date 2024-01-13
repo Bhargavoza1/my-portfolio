43599b502700fa4430546f7b6c4320f275b3e96d
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2024-01-13 13:58:32 +0530

    pagination

--- src/app/(section)/blogs/page.tsx
+++ src/app/(section)/blogs/page.tsx
@@ -7,13 +7,17 @@ import Link from "next/link";
 
 
 import type {Metadata} from 'next'
+import {getBlogPostsMeta} from "@/lib/posts";
 
 export const metadata: Metadata = {
     title: "Bhargav's Blogs",
     description: 'hey you can check here what i am exploring next ',
 }
 
-function About(): React.JSX.Element {
+export default async function page({searchParams}:{searchParams:any}): Promise<React.JSX.Element> {
+    const posts = await getBlogPostsMeta(  searchParams.page )
+   // console.log(posts?.totalPages)
+    const totalpage = posts?.totalPages
     return (
         <div className='mt-14 md:mt-24 px-4 md:px-6  max-w-[1700px] mx-auto    pt-4 md:pb-4 sm:px-6  lg:px-8'>
 
@@ -27,10 +31,29 @@ function About(): React.JSX.Element {
                     stories.
                 </div>
             </div>
-            <BlogPosts/>
+            <BlogPosts posts={posts} />
+
+
+            {/* Add the pagination navigation */}
+            <nav aria-label="Page navigation example">
+                <ul className="list-style-none flex">
+                    {Array.from({length: totalpage}, (_, i) => i + 1).map((page) => (
+                        <li key={page}>
+                            <Link href={`/blogs?page=${page}`}>
+                                <div
+                                    className={`relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white ${page === parseInt(searchParams.page) ? 'bg-neutral-100 dark:bg-neutral-700' : ''}`}
+                                >
+                                    {page}
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+
         </div>
 
     );
 }
 
-export default About;
